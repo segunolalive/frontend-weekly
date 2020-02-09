b678bcff7af76a4fa1991486781b686e43043c50
Segun <segunolalive@gmail.com>
2020-02-09 11:05:40 +0100

    use header tag in hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,10 +4,10 @@ import Img from 'gatsby-image'
 import styles from './hero.module.css'
 
 export default () => (
-  <div className={styles.hero}>
+  <header className={styles.hero}>
     <div className={styles.heroDetails}>
       <h1 className={styles.heroHeadline}>Front-End Weekly</h1>
       <p>all things front-end development</p>
     </div>
-  </div>
+  </header>
 )
