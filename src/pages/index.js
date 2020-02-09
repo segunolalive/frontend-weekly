de82697a5a621635093c55184150422759cfba37
Segun <segunolalive@gmail.com>
2020-02-09 09:42:13 +0100

    remove hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,13 @@ import Session from '../components/session'
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
     const sessions = get(this, 'props.data.allContentfulSession.edges')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <Hero data={author.node} />
+          <Hero />
           <div className="wrapper">
             <h2 className="section-headline">Recent videos</h2>
             <ul className="article-list">
@@ -58,30 +57,6 @@ export const pageQuery = graphql`
           title
           videoUrl
           sessionDate
-        }
-      }
-    }
-    # remove this
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
         }
       }
     }
